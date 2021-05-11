b061f474922ea1a0daa221fbc066690bf7182dd4
azsx92 <azsx92@gmail.com>
2021-05-12 05:43:26 +0900

    test git commit

---
+++ src/test/java/com/jojoldu/book/springboot/HelloControllerTest.java
@@ -0,0 +1,32 @@
+package com.jojoldu.book.springboot;
+
+import com.jojoldu.book.springboot.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.StatusAssertions;
+import org.springframework.test.web.reactive.server.XpathAssertions;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴한다() throws Exception {
+        String hello ="hello";
+
+
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+
+    }
+
+
+}
